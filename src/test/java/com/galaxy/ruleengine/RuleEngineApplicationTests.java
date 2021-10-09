3b4dc469765db82655cedac62f3c2469d8b1d30b
yanghaolei <yanghaolei@xiaohongshu.com>
2021-10-09 17:46:45 +0800

    feat(dev): init repo

---
+++ src/test/java/com/galaxy/ruleengine/RuleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxy.ruleengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
